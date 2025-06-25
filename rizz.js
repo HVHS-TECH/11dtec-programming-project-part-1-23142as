071f24778d2d9d8616388f87e48491a79a73bd90
23142as <23142as@hvhs.school.nz>
2025-06-25 00:25:47 +0000

    I have to do the money and the maty thing

--- rizz.js
+++ rizz.js
@@ -1,39 +1,47 @@
-function button () {
-getName();
-getFavColour();
-getFaAge();
-getMoney();
+function button() {
+  getName();
+  getFavColour();
+  getSize();
+  getMoney();
 
 }
 
-function getName () {
-getName = prompt("What is your name?")
-alert("Your name is: " + getName)
+function getName() {
+  getName = prompt("What is your name?")
+  alert("Your name is: " + getName)
 }
 let userFavColor
 let userFavColorValid = false;
-function getFavColour () {
+function getFavColour() {
 
   userFavColor = prompt("What would you like to buy?");
-if (userFavColor == "Nike Tech" || userFavColor == "Nike Fleece CS Crew" || userFavColor == "Nike Hoodie" || userFavColor == "Nike Fleece Sweatpants" || userFavColor == "Nike T-shirts" || userFavColor == "Nike Cardinal Nylon Shorts"){
-  alert("You are looking for: " + userFavColor)
-} else { alert("That is not an option")
+  if (userFavColor == "Nike tech" || userFavColor == "Nike crew" || userFavColor == "Nike hoodie" || userFavColor == "Nike sweatpants" || userFavColor == "Nike t-shirts"
+    || userFavColor == "Nike shorts" || userFavColor == "nike tech" || userFavColor == "nike crew" || userFavColor == "nike hoodie" || userFavColor == "nike sweatpants"
+    || userFavColor == "nike t-shirts" || userFavColor == "nike shorts") {
+    alert("You are looking for: " + userFavColor)
+  } else {
+    alert("That is not an option")
+  }
+}
+
+function getSize() {
+  userSize = prompt("These are the size available\n XS\n S\n m\n L\n XL\n XXL\n Choose the one that will fit you.")
+  if (userSize == "XS" || userSize == "S" || userSize == "L" || userSize == "XL" || userSize == "XXL" || userSize == "xs" || userSize == "s" || userSize == "l" || userSize == "xl" || userSize == "xxl") {
+    alert("So you want: " + userSize + " size.")
+  } else {
+    alert("That is not an option")
+  }
 
 }
 
-function getFavAge () {
-userFavAge = prompt("These are the size available\n XS\n S\n m\n L\n XL\n XXL\n Choose the one that will fit you.")
-alert("So you want: " + userFavAge + " size.")
+function getMoney() {
+  clientMoney = prompt(" how much money do you have?");
+
+  if (clientMoney < 4) {
+    alert("Your pocket money is " + clientMoney + " dollars.\n Sorry you CAN'T afford a chocolate bar")
+  } else {
+    alert("Your pocket money is " + clientMoney + " dollars.\n You can afford a chocolate bar")
+
+  }
 }
 
-function getMoney () {
-clientMoney = prompt(" how much money do you have?");
-
-if (clientMoney < 4){
-  alert("Your pocket money is " +clientMoney+ " dollars.\n Sorry you CAN'T afford a chocolate bar")
-} else { 
-alert("Your pocket money is " +clientMoney+ " dollars.\n You can afford a chocolate bar")
-
-}
-}
-}
