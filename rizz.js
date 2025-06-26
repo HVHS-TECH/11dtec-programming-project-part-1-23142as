ae8afdd98dc7d19548689c6ca97e592b3812aa98
23142as <23142as@hvhs.school.nz>
2025-06-26 01:30:08 +0000

    I am working on getting my buttons working

--- rizz.js
+++ rizz.js
@@ -1,47 +1,26 @@
-function button() {
-  getName();
-  getFavColour();
-  getSize();
-  getMoney();
 
+
+let userName; 
+let userChoice;
+let userSeats;
+let carArray = [ "Honda CR-Z", "Honda CR-V", "Honda Civic", "Honda Freed", "Honda CR-V", "Honda Odyssey:"];
+let choice;
+let userDays;
+let answer = 0;
+var ten = 10;
+
+function start() {
+    userName = prompt("I'm Adarsh.\nWhats your name?");
+    alert("Hello " + userName + " welcome to Honda's car rentals");
+    choice = prompt("How many seats would you the car to have?\n Sorry Honda doesn't have any 3 seater car modal,");
+    while (choice < 0 || choice > 8) {
+        choice = prompt("We have no cars with " + choice + " seats.\nWe have ones with 1-8 seats");
+    }
+    userSeats = choice;
+    alert("You want " + choice + " seats in your car.\nWe have a " + carArray[choice] + " for you.");
+    userDays = prompt("How many days do you want to rent " + carArray[choice] + " for?");
+    alert("You want to rent the car for " + userDays + " days.");
+    answer = Number(userSeats) * Number(userDays) * ten;
+    alert("This " + carArray[choice] + " will cost you $" + answer + ".");
+    alert("You want a " + carArray[choice] + " for " + userDays + " days.\nThis will cost you $" + answer + ".");
 }
-
-function getName() {
-  getName = prompt("What is your name?")
-  alert("Your name is: " + getName)
-}
-let userFavColor
-let userFavColorValid = false;
-function getFavColour() {
-
-  userFavColor = prompt("What would you like to buy?");
-  if (userFavColor == "Nike tech" || userFavColor == "Nike crew" || userFavColor == "Nike hoodie" || userFavColor == "Nike sweatpants" || userFavColor == "Nike t-shirts"
-    || userFavColor == "Nike shorts" || userFavColor == "nike tech" || userFavColor == "nike crew" || userFavColor == "nike hoodie" || userFavColor == "nike sweatpants"
-    || userFavColor == "nike t-shirts" || userFavColor == "nike shorts") {
-    alert("You are looking for: " + userFavColor)
-  } else {
-    alert("That is not an option")
-  }
-}
-
-function getSize() {
-  userSize = prompt("These are the size available\n XS\n S\n m\n L\n XL\n XXL\n Choose the one that will fit you.")
-  if (userSize == "XS" || userSize == "S" || userSize == "L" || userSize == "XL" || userSize == "XXL" || userSize == "xs" || userSize == "s" || userSize == "l" || userSize == "xl" || userSize == "xxl") {
-    alert("So you want: " + userSize + " size.")
-  } else {
-    alert("That is not an option")
-  }
-
-}
-
-function getMoney() {
-  clientMoney = prompt(" how much money do you have?");
-
-  if (clientMoney < 4) {
-    alert("Your pocket money is " + clientMoney + " dollars.\n Sorry you CAN'T afford a chocolate bar")
-  } else {
-    alert("Your pocket money is " + clientMoney + " dollars.\n You can afford a chocolate bar")
-
-  }
-}
-
