6810e0aacfd70a0cad59c1bdc504db23c8f553b1
23142as <23142as@hvhs.school.nz>
2025-06-27 03:01:25 +0000

    Jun 27, 2025, 3:01 AM

--- rizz.js
+++ rizz.js
@@ -10,11 +10,11 @@ let answer = 0;
 var ten = 10;
 
 function start() {
-    userName = prompt("I'm Adarsh.\nWhats your name?");
+    userName = prompt("I'm Adarsh. I will be helping you select your car?\nWhats your name?");
     alert("Hello " + userName + " welcome to Honda's car rentals");
-    choice = prompt("How many seats would you the car to have?\n Sorry Honda doesn't have any 3 seater car modal,");
+    choice = prompt("How many seats would you the car to have from between 1-5 seats?\n Sorry we are in a shortage for the 6-7 seater car.");
     while (choice < 0 || choice > 8) {
-        choice = prompt("We have no cars with " + choice + " seats.\nWe have ones with 1-8 seats");
+        choice = prompt("We have no cars with " + choice + " seats.\nWe have ones with 1-5 seats");
     }
     userSeats = choice;
     alert("You want " + choice + " seats in your car.\nWe have a " + carArray[choice] + " for you.");
